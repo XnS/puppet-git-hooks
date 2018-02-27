80120e5719c9f42d5f688772616bde01a26e4a67
Mattias Giese <mattiasgiese@posteo.net>
2018-02-27 17:50:21 +1300

    Try to determine a usable yaml linter, provide a minimal one using ruby if nothing is found

--- commit_hooks/yaml_syntax_check.sh
+++ commit_hooks/yaml_syntax_check.sh
@@ -2,31 +2,49 @@
 
 # This script expects $1 to be passed and for $1 to be the filesystem location
 # to a yaml file for which it will run syntax checks against.
+LINTER=''
+
+for linter in yaml-lint yamllint; do
+  which $linter &>/dev/null && LINTER="$linter"
+done
+
+# Use a simple default if no linter is found
+if [ -z "$LINTER" ]; then
+  yaml_check ()
+  {
+      if [ $# -lt 1 ]; then
+          echo Usage: yaml_check file;
+          return;
+      fi;
+      ruby -e "require 'yaml';require 'pp';pp YAML.load_file('$1')"
+  }
+  LINTER=yaml_check
+fi
 
 syntax_errors=0
 error_msg=$(mktemp /tmp/error_msg_yaml-syntax.XXXXX)
 
 if [[ $2 ]]; then
-    module_path="${1##*$2}"
+  module_path="${1##*$2}"
 else
-    module_path=$1
+  module_path=$1
 fi
 
 # Check YAML file syntax
 $ERRORS_ONLY || echo -e "$(tput setaf 6)Checking yaml syntax for $module_path...$(tput sgr0)"
-yaml-lint $1 > "$error_msg"
+$LINTER $1 > "$error_msg"
 if [ $? -eq 127 ]; then
-  echo -e "$(tput setaf 1)Error: yaml-lint not found, please install a linter$(tput sgr0)"
+  echo -e "$(tput setaf 1)No linter found, please install a linter like yaml-lint oder yamllint$(tput sgr0)"
 elif [ $? -ne 0 ]; then
-    sed -e "s/^/$(tput setaf 1)/" -e "s/$/$(tput sgr0)/" "$error_msg"
-    syntax_errors=$((syntax_errors + 1))
-    echo -e "$(tput setaf 1)Error: yaml syntax error in $module_path (see above)$(tput sgr0)"
+  sed -e "s/^/$(tput setaf 1)/" -e "s/$/$(tput sgr0)/" "$error_msg"
+  syntax_errors=$((syntax_errors + 1))
+  echo -e "$(tput setaf 1)Error: yaml syntax error in $module_path (see above)$(tput sgr0)"
 fi
 rm -f "$error_msg"
 
 if [[ $syntax_errors -ne 0 ]]; then
-    echo -e "$(tput setaf 1)Error: $syntax_errors syntax error(s) found in hiera yaml. Commit will be aborted.$(tput sgr0)"
-    exit 1
+  echo -e "$(tput setaf 1)Error: $syntax_errors syntax error(s) found in hiera yaml. Commit will be aborted.$(tput sgr0)"
+  exit 1
 fi
 
 exit 0
